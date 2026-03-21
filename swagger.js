c3a13a879c77f1be837105f417049c74bbb0e31d
KwameKingsley <Kwartkingsley@gmail.com>
2026-03-21 08:51:01 +0000

    swagger updates

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Users API',
         description: 'Users API for CSE 341 Web Services'
     },
-    host: 'https://github.com/KwameKingsley/cse-341-webServices',
+    host: 'github.com/KwameKingsley/cse-341-webServices',
     schemes: ['https']
 };
 
